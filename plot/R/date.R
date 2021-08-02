badc258e047e19d025e2e52825d80a02a8ad230c
Benoit Pimpaud <pimpaudben@gmail.com>
2021-08-02 17:03:14 +0200

    [develop] fix typo

--- plot/R/date.R
+++ plot/R/date.R
@@ -1,6 +1,6 @@
 library(lubridate)
 
 find_last_month_year <- function(){
-    date_minus_month <- add_with_rollback(ymd(today), months(-1))
+    date_minus_month <- add_with_rollback(ymd(today()), months(-1))
     return(format(date_minus_month, "%Y-%m"))
 }
